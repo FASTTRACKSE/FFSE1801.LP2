d0878da32a9aa62b1ff757eb1bf687da3953ae71
fanguyen02 <fanguyen2k@gmail.com>
2018-11-07 12:58:37 +0700

    hh
    jh

---
+++ projects/ffse1801006/js/HOME.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.carousel').carousel({interval: 3000});
+    $('.product-slide').carousel({interval: 4000})
+
+    $(window).scroll(function() {
+        let scroll = $("html").scrollTop()
+        console.log($("html").scrollTop())
+
+        if(scroll>0){
+           $(".menu").addClass("menu-scroll") 
+           $(".mini-menu").addClass("mini-menu-scroll")
+           
+        }else{
+            $(".menu").removeClass("menu-scroll")
+            $(".mini-menu").removeClass("mini-menu-scroll")
+        }
+    })
+    $("#search-button").click(function(){
+        $("#search-bar").slideDown(300)
+        $("#search-bar").fadeIn(300)
+    })
+    $("#search-close").click(function(){
+        $("#search-bar").slideUp(300)
+        $("#search-bar").fadeOut(300)
+    })
+    
+
+});
