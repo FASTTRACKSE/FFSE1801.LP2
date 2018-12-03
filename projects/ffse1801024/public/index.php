c7c477434ed15d6acd2ffd62074051c555729506
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 04:04:42 +0700

    up

--- projects/ffse1801024/public/index.php
+++ projects/ffse1801024/public/index.php
@@ -1,23 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');  
-$servername = "fdb24.awardspace.net";
-$username = "2900176_xyzddostxx";
-$password = "Vanan1998tk";
-$dbname = "2900176_xyzddostxx";
-$port = "3306";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname,$port);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = 'INSERT INTO Cookie (datacookie)
-VALUES ("'.$_REQUEST["q"].'")';
-
-if (mysqli_query($conn, $sql)) {
-} else {
-}
-mysqli_close($conn);
+session_start();
+session_id();
 ?>
