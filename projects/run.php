47db7bbd6e599aba2e1a3ab2e8ae4e3e9bae994e
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 17:18:17 +0700

    up

--- projects/run.php
+++ projects/run.php
@@ -3,18 +3,22 @@ $dir    = '/tmp';
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
 
+print_r("<pre>");
 print_r($files1);
 print_r($files2);
+print_r("</pre>");
 $dir    = '/';
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
-
+print_r("<pre>");
 print_r($files1);
 print_r($files2);
+print_r("</pre>");
 $dir    = '../';
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
-
+print_r("<pre>");
 print_r($files1);
 print_r($files2);
+print_r("</pre>");
 ?>
