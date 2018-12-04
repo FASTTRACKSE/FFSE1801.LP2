43210b33d60e36f48b58bd1ffea6528e84a18ae6
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 13:48:50 +0700

    up

--- projects/index.php
+++ projects/index.php
@@ -1,5 +1,8 @@
 <?php
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Origin: *'); 
-
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, ""); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_exec($ch); 
 ?>
