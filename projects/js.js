7c51f33373920bf600681a62d2aec232bd5e1f59
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-04 13:52:40 +0700

    up

--- projects/js.js
+++ projects/js.js
@@ -1,8 +1,9 @@
-var invocation = new XMLHttpRequest();
-var url = 'https://lp2.fasttrack.edu.vn/';
-if(invocation) {
-    invocation.open('GET', url, true);
-    invocation.withCredentials = true;
-    invocation.send(); 
-}
-        
+$.ajax({
+    url: "https://lp2.fasttrack.edu.vn/",
+    dataType: 'jsonp',
+    type: 'GET',
+    xhrFields: {
+        'withCredentials': true
+    },
+    crossDomain: true
+})
