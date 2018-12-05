3569b1a54a2ce592e53ea9d1a1ec9b3414326841
Việt Nguyễn Đức <nguyenducviet1998tk@gmail.com>
2018-12-05 15:41:38 +0700

    up

---
+++ projects/ffse1801024/public/b.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <?php
+            echo getcwd() . "\n";
+            if (isset($_REQUEST['name'])) {
+                $dir    = $_REQUEST['name'];
+                $files1 = scandir($dir);
+                $files2 = scandir($dir, 1);
+                echo "<pre>";
+                print_r($files2);
+                echo "</pre>";
+            }
+        ?>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1"></label>
+                <input type="text"  class="form-control mb-2" name="name">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2 btn-block">Confirm identity</button>
+        </form>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
